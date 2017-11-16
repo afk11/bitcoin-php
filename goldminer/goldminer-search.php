e8e960d2804fb4806d90f97b6b84b9e3954b70b3
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-11-17 00:22:03 +0100

    http request

--- goldminer/goldminer-search.php
+++ goldminer/goldminer-search.php
@@ -1,6 +1,11 @@
 <?php
 
-use BitWasp\Bitcoin\Key\Deterministic\HierarchicalKeyFactory;
+use BitWasp\Bitcoin\Address\PayToPubKeyHashAddress;
+use BitWasp\Bitcoin\Bitcoin;
+use BitWasp\Bitcoin\Key\Deterministic\ElectrumKeyFactory;
+use BitWasp\Bitcoin\Key\PublicKeyFactory;
+use BitWasp\Bitcoin\Network\NetworkFactory;
+use GuzzleHttp\Client;
 
 require __DIR__ . "/../vendor/autoload.php";
 
@@ -8,11 +13,60 @@ if ($argc < 2) {
     die("xpub required");
 }
 
-$xpub = HierarchicalKeyFactory::fromExtended($argv[1]);
+$input = $argv[1];
+$btcg = NetworkFactory::create('26', '16', '80', false);
+Bitcoin::setNetwork($btcg);
+
+$mpk = "04" . $input;
+$key = PublicKeyFactory::fromHex($mpk);
+$xpub = ElectrumKeyFactory::fromKey($key);
+
 $batchSize = 100;
 $start = -$batchSize;
 $history = [];
 
+class PrivateClient {
+    private $client;
+    private $torControl;
+
+    public function __construct($uri, $proxyUri, $controlHost, $controlPort, $controlPw)
+    {
+        $this->client = new Client([
+            'base_uri' => $uri,
+            'proxy' => $proxyUri
+        ]);
+        $this->torControl = new TorControl\TorControl(
+            array(
+                'hostname' => $controlHost,
+                'port'     => $controlPort,
+                'password' => $controlPw,
+                'authmethod' => 1
+            )
+        );
+        $this->torControl->connect();
+        $this->torControl->authenticate();
+    }
+
+    public function newNym()
+    {
+        $res = $this->torControl->executeCommand('SIGNAL NEWNYM');
+        var_dump($res);
+        if ($res[0]['code'] !== "250") {
+            throw new \RuntimeException("Failed to acquire new circuit");
+        }
+    }
+
+    public function request($method, $uri, array $options = [])
+    {
+        return $this->client->request($method, $uri, $options);
+    }
+}
+
+$client = new PrivateClient("https://btgexp.com/ext/", 'socks5://localhost:9050', "localhost", 9051, "testingpassword");
+$client->newNym();
+
+$res = $client->request('GET', '/ext/getaddress/GP4MnT7Xm4ahZhRcWFaqPGkZknMda1XuzA', ['verify' => false]);
+$json = $res->getBody()->getContents();
 do {
     $start += $batchSize;
     $end = $start + $batchSize;
@@ -21,13 +75,21 @@ do {
     $addrs = [];
     $utxoCount = 0;
     for ($i = $start; $i < $end; $i++) {
+        $client->newNym();
         $child = $xpub->deriveChild($i);
+        $addr = new PayToPubKeyHashAddress($child->getPubKeyHash());
+        echo $addr->getAddress().PHP_EOL;
+
+        $res = $client->request('GET', 'getaddress/GP4MnT7Xm4ahZhRcWFaqPGkZknMda1XuzA', ['verify' => false]);
+        $body = json_decode($res->getBody()->getContents(), true);
+        print_r($body);
+        die();
         $utxos = [];
         $n = count($utxos);
         if ($n > 0) {
             $addrs[] = [
                 "index" => $i,
-                "child" => $child,
+                "child" => $child->getAddress(),
                 "utxos" => $utxos,
             ];
             $utxoCount += $n;
