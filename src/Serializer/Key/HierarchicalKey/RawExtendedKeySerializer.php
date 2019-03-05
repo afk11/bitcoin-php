74e0e2c4cde7553a671477730a6ad798c8225546
Thomas Kerin <thomas.kerin@bitmaintech.com>
2019-03-05 19:17:54 +0000

    RawExtendedKeySerializer: use pack instead of buffertools types

--- src/Serializer/Key/HierarchicalKey/RawExtendedKeySerializer.php
+++ src/Serializer/Key/HierarchicalKey/RawExtendedKeySerializer.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace BitWasp\Bitcoin\Serializer\Key\HierarchicalKey;
 
 use BitWasp\Bitcoin\Crypto\EcAdapter\Adapter\EcAdapterInterface;
-use BitWasp\Bitcoin\Serializer\Types;
 use BitWasp\Buffertools\Buffer;
 use BitWasp\Buffertools\BufferInterface;
 use BitWasp\Buffertools\Exceptions\ParserOutOfRange;
@@ -19,42 +18,12 @@ class RawExtendedKeySerializer
     private $ecAdapter;
 
     /**
-     * @var \BitWasp\Buffertools\Types\ByteString
-     */
-    private $bytestring4;
-
-    /**
-     * @var \BitWasp\Buffertools\Types\Uint8
-     */
-    private $uint8;
-
-    /**
-     * @var \BitWasp\Buffertools\Types\Uint32
-     */
-    private $uint32;
-
-    /**
-     * @var \BitWasp\Buffertools\Types\ByteString
-     */
-    private $bytestring32;
-
-    /**
-     * @var \BitWasp\Buffertools\Types\ByteString
-     */
-    private $bytestring33;
-
-    /**
      * RawExtendedKeySerializer constructor.
      * @param EcAdapterInterface $ecAdapter
      */
     public function __construct(EcAdapterInterface $ecAdapter)
     {
         $this->ecAdapter = $ecAdapter;
-        $this->bytestring4 = Types::bytestring(4);
-        $this->uint8 = Types::uint8();
-        $this->uint32 = Types::uint32();
-        $this->bytestring32 = Types::bytestring(32);
-        $this->bytestring33 = Types::bytestring(33);
     }
 
     /**
@@ -65,12 +34,12 @@ class RawExtendedKeySerializer
     public function serialize(RawKeyParams $keyParams): BufferInterface
     {
         return new Buffer(
-            pack("H*", $keyParams->getPrefix()) .
-            $this->uint8->write($keyParams->getDepth()) .
-            $this->uint32->write($keyParams->getParentFingerprint()) .
-            $this->uint32->write($keyParams->getSequence()) .
-            $this->bytestring32->write($keyParams->getChainCode()) .
-            $this->bytestring33->write($keyParams->getKeyData())
+            pack(
+                "H*CNN",
+                $keyParams->getPrefix(), $keyParams->getDepth(), $keyParams->getParentFingerprint(), $keyParams->getSequence()
+            ) .
+            $keyParams->getChainCode()->getBinary() .
+            $keyParams->getKeyData()->getBinary()
         );
     }
 
@@ -82,13 +51,15 @@ class RawExtendedKeySerializer
     public function fromParser(Parser $parser): RawKeyParams
     {
         try {
+            $prefix = $parser->readBytes(4)->getHex();
+            $p = unpack("C1depth/NparFpr/Nsequence", $parser->readBytes(1+4+4)->getBinary());
             return new RawKeyParams(
-                $this->bytestring4->read($parser)->getHex(),
-                (int) $this->uint8->read($parser),
-                (int) $this->uint32->read($parser),
-                (int) $this->uint32->read($parser),
-                $this->bytestring32->read($parser),
-                $this->bytestring33->read($parser)
+                $prefix,
+                $p['depth'],
+                $p['parFpr'],
+                $p['sequence'],
+                $parser->readBytes(32),
+                $parser->readBytes(33)
             );
         } catch (ParserOutOfRange $e) {
             throw new ParserOutOfRange('Failed to extract HierarchicalKey from parser');
